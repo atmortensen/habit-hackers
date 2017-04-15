937582f41a507f68b4afc2c5578918dc999b9929
Alexand Mortensen <alextmortensen@gmail.com>
2017-04-14 19:46:13 -0600

    Modal Finished

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,19 +6,36 @@ export default class Modal extends Component {
 		super(props)
 
 		this.state = {
-			opacity: props.display
+			opacity: 0,
+			pointerEvents: 'none',
+			marginTop: '-50px'
 		}
+
 		this.closeModal = this.closeModal.bind(this)
+		this.openModal = this.openModal.bind(this)
 	}
 
 	closeModal(){
-		this.setState({opacity: 'none'})
+		this.setState({
+			opacity: 0, 
+			pointerEvents: 'none',
+			marginTop: '-50px'
+		})
 	}
 
-	componentDidMount(){
-		const modal = document.querySelector('.modal-wrap')
-		if(modal.style.opacity === '0'){
-			modal.style.display = 'none'
+	openModal(){
+		this.setState({
+			opacity: 1, 
+			pointerEvents: 'all', 
+			marginTop: 0
+		})
+	}
+
+	componentWillReceiveProps(newProps) {
+		if(newProps.display===1){
+			this.openModal()
+		} else {
+			this.closeModal()
 		}
 	}
 
@@ -26,8 +43,15 @@ export default class Modal extends Component {
 		return (
 			<div 
 				className="modal-wrap"
-				style={{opacity: this.state.opacity}}>
-				<div className="modal">
+				style={{
+					opacity: this.state.opacity, 
+					pointerEvents: this.state.pointerEvents
+				}}>
+				<div 
+					className="modal"
+					style={{
+						marginTop: this.state.marginTop, 
+					}}>
 					<div 
 						className="close"
 						onClick={this.closeModal}>
