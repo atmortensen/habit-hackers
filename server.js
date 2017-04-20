aee6eceb334bf1baec2cce7bdf4fc978569cfe70
Alexander Mortensen <alextmortensen@gmail.com>
2017-04-20 08:59:47 -0600

    Serve only react files

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
 
-const authenticate = require('./server/jwtAuth.js')
-const databaseCtrl = require('./server/databaseCtrl.js')
+// const authenticate = require('./server/jwtAuth.js')
+// const databaseCtrl = require('./server/databaseCtrl.js')
 
 app.set('port', (process.env.PORT || 3001))
-app.use(bodyParser.json())
-app.use(cors())
+// app.use(bodyParser.json())
+// app.use(cors())
 app.use(express.static(__dirname + '/build'))
 
-app.get('/api/habits', authenticate, databaseCtrl.findAll)
-app.post('/api/habits', authenticate, databaseCtrl.create)
-app.delete('/api/habits/:id', authenticate, databaseCtrl.remove)
+// app.get('/api/habits', authenticate, databaseCtrl.findAll)
+// app.post('/api/habits', authenticate, databaseCtrl.create)
+// app.delete('/api/habits/:id', authenticate, databaseCtrl.remove)
 
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/build/index.html') 
