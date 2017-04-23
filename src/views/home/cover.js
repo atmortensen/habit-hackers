b89b0f8de1e3de3eed9659f758104e7379beb4a2
Alexander Mortensen <alextmortensen@gmail.com>
2017-04-23 16:43:35 -0600

    Mobil Responsive and Build

--- src/views/home/cover.js
+++ src/views/home/cover.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import auth from '../../helpers/auth0'
 
 export default class Cover extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Cover extends Component {
 	      	<h1>Habit Hackers</h1>
 	      	<div className="break"></div>
 	      	<h2>Work as a team to create good habits or break bad ones.</h2>
+          <button onClick={auth.login}>Start Now!</button>
       	</div>
       </div>
     )
